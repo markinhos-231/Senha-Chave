c4d1c8b33b58b2c5ac24bce94a1a15afcb539f9f
markinhos-231 <144041090+markinhos-231@users.noreply.github.com>
2025-08-12 22:03:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const senhaInput = document.getElementById("senha");
+const forcaSenha = document.getElementById("forcaSenha");
 
+senhaInput.addEventListener("input", () => {
+    const senha = senhaInput.value;
+    let forca = 0;
+
+    if (senha.length >= 8) forca++;
+    if (/[A-Z]/.test(senha)) forca++;
+    if (/[0-9]/.test(senha)) forca++;
+    if (/[^A-Za-z0-9]/.test(senha)) forca++;
+
+    if (forca === 0) {
+        forcaSenha.textContent = "Digite uma senha...";
+        forcaSenha.style.background = "#ccc";
+    } else if (forca <= 2) {
+        forcaSenha.textContent = "Senha fraca 😬";
+        forcaSenha.style.background = "#ff4d4d";
+    } else if (forca === 3) {
+        forcaSenha.textContent = "Senha média 🙂";
+        forcaSenha.style.background = "#ffcc00";
+    } else {
+        forcaSenha.textContent = "Senha forte 💪";
+        forcaSenha.style.background = "#4CAF50";
+    }
+});
